72d57f6987a4593fe2c34f1f27999116642d7c7f
Sherif Eldeeb <archeldeeb@gmail.com>
2012-09-19 21:03:45 +0300

    Password using MD5
    Password MD5 hash is stored inside the binary, computed from user input
    then compared...  instead of the silly string compare which could be
    extracted from the binary by strings....
    Using windows CryptoAPI

--- galactus/galactus.cpp
+++ galactus/galactus.cpp
@@ -3,6 +3,9 @@
 #include <tchar.h>
 #include <wchar.h>
 #include <io.h>
+char cMD5PassHash[] = "\xcc\x03\xe7\x47\xa6\xaf\xbb\xcb\xf8\xbe\x76\x68\xac\xfe\xbe\xe5"; // md5(test123) + '\0' = 17
+BOOL GetMD5Hash(char *buffer,			//input buffer
+				BYTE *byteFinalHash);	//output buffer 
 
 u_long FileSizeByName(wchar_t *sFileName)
 {
@@ -103,19 +106,38 @@ int ListDirectoryContents(const wchar_t *sDir)
 	return 0; 
 } 
 
+
 //int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmdLine, int iCmdShow)
 int _tmain(int argc, TCHAR *argv[])
 {
-	wchar_t cUserInput[16]={0};
+	char cUserInput[32]={0};
 	wprintf_s(L"Password:\n");
-	fgetws(cUserInput, sizeof(cUserInput), stdin);
-	wprintf_s(L"Password is: %s\n", cUserInput);
+	fgets(cUserInput, sizeof(cUserInput), stdin);
+	printf_s("Password is: %s\n", cUserInput);
+	cUserInput[strlen(cUserInput)-1] = '\0'; //replace \n with \0
+	BYTE byteHashbuffer[16] = {0}; //where the computed hash will be stored
+	GetMD5Hash(cUserInput, byteHashbuffer);
+#ifdef _DEBUG
+	for( int i=0; i < 16; i++)
+	{
+		wprintf_s(L"%02x",byteHashbuffer[i]); //the %02x means output 2 digit hex, with leading zeros.
+	}
+#endif
+
+	
+	int i = memcmp(byteHashbuffer,cMD5PassHash,16);
+	
+	/*
 	int i = wcscmp(cUserInput,L"test123\n");
+	*/
 	if(i!=0)
 	{
-		wprintf_s(L"\nBad password!\n");
-		exit(1);
+	wprintf_s(L"Bad password!\n");
+	exit(1);
 	};
+
+	
+
 
 	wchar_t cDstDir[MAX_PATH] = {0};
 	if(argc != 2)
@@ -131,3 +153,66 @@ int _tmain(int argc, TCHAR *argv[])
 	ListDirectoryContents(cDstDir);
 	return 0;
 }
+ 
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//code taken from URI below... well, I modified it a LOT, go for the link to see how it looked like...
+//http://nagareshwar.securityxploded.com/2010/10/22/cryptocode-generate-md5-hash-using-windows-cryptography-library/
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Compute the MD5 checksum for input buffer
+//
+BOOL GetMD5Hash(char *buffer,             //input buffer
+				BYTE *byteFinalHash)    //ouput hash buffer
+{
+	BOOL bResult = FALSE;
+	HCRYPTPROV hProv = 0;
+	HCRYPTHASH hHash = 0;
+
+	// Get handle to the crypto provider
+	if (!CryptAcquireContext(&hProv, NULL, MS_DEF_PROV, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+	{
+		printf("\nCryptAcquireContext failed, Error=0x%.8x", GetLastError());
+		return FALSE;
+	}
+
+	//Specify the Hash Algorithm here
+	if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
+	{
+		printf("\nCryptCreateHash failed,  Error=0x%.8x", GetLastError());
+		goto EndHash;
+	}
+
+	//Create the hash with input buffer
+	if (!CryptHashData(hHash, (const BYTE*) buffer, strlen(buffer), 0))
+	{
+		printf("\nCryptHashData failed,  Error=0x%.8x", GetLastError());
+		goto EndHash;
+	}
+	DWORD dwTemp = 16;
+	DWORD *pdwDataLen = &dwTemp;
+
+
+	//Now get the computed hash
+	if (CryptGetHashParam(hHash, HP_HASHVAL, byteFinalHash, pdwDataLen, 0))
+	{
+		printf("\n[*]Hash Computed successfully:\n");
+		bResult = TRUE; 
+	}
+	else
+	{
+		printf("\nCryptGetHashParam failed,  Error=0x%.8x", GetLastError());
+	}
+
+
+EndHash:
+
+	if( hHash )
+		CryptDestroyHash(hHash);
+
+	if( hProv )
+		CryptReleaseContext(hProv, 0);
+
+	return bResult; 
+}  
